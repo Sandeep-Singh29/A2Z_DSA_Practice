16ad2f65166b3180b30d9d100d4eb13a42165243
Sandeep-Singh29 <singh29july95@gmail.com>
2024-08-08 20:09:48 +0530

    code majority

---
+++ src/com/practice/leetcode/MajorityElement_169.java
@@ -0,0 +1,45 @@
+package com.practice.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement_169 {
+    public static void main(String[] args) {
+        int[] arr = {6, 5, 5};
+//        int ele = findMajorityElement(arr);
+//        System.out.println(ele);
+        int data = mooreVotingAlgo(arr);
+        System.out.println(data);
+    }
+
+    private static int mooreVotingAlgo(int[] nums) {
+        int ele=0;
+        int count = 0;
+        for (int v : nums) {
+            if(count==0){
+                ele = v;
+                count++;
+            } else if (ele!=v) {
+                count--;
+            }else {
+                count++;
+            }
+        }
+        return ele;
+    }
+
+    private static int findMajorityElement(int[] nums) {
+        int n = (nums.length - 1) / 2;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int v : nums) {
+            map.put(v, map.getOrDefault(v, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> m : map.entrySet()) {
+            if (m.getValue() > n) {
+                return m.getKey();
+            }
+        }
+        return 0;
+    }
+}
+
