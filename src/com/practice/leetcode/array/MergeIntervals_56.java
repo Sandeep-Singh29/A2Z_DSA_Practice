7f30e935f6cc793703daaa659ead6e6293f845e2
Sandeep-Singh29 <singh29july95@gmail.com>
2024-08-21 23:31:45 +0530

    day 5 night code

--- src/com/practice/leetcode/array/MergeIntervals_56.java
+++ src/com/practice/leetcode/array/MergeIntervals_56.java
@@ -1,34 +1,46 @@
 package com.practice.leetcode.array;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class MergeIntervals_56 {
 
-    public static int[][] merge(int[][] arr) {
-        int r = arr.length;
-        int c = arr[0].length;
-        List<List<Integer>> list = new ArrayList<>();
-        Arrays.sort(arr);
-        for (int i = 0; i < r; i++) {
-               int start = arr[i][0];
-               int end = arr[i][1];
-             if(list.isEmpty()){
-                 list.add(Arrays.asList(start,end));
-             }
-             if(!list.isEmpty()){
-                 if(list.get(i).get(1)>arr[i][0]){
-//                     list.add(list);
-                 }
-             }
+    public static int[][] merge(int[][] intervals) {
+        if (intervals.length <= 1)
+            return intervals;
+        Arrays.sort(intervals, Comparator.comparingInt(i -> i[0]));
+        List<int[]> result = new ArrayList<>();
+        int[] newInterval = intervals[0];
+        result.add(newInterval);
+        for (int[] interval : intervals) {
+            if (interval[0] <= newInterval[1])
+                newInterval[1] = Math.max(newInterval[1], interval[1]);
+            else {
+                newInterval = interval;
+                result.add(newInterval);
+            }
         }
-        return null;
+        return result.toArray(new int[result.size()][]);
+    }
+
+    public static List<List<Integer>> mergeOverlappingIntervals(int[][] arr) {
+        int n = arr.length;
+        Arrays.sort(arr,Comparator.comparingInt(i-> i[0]));
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (ans.isEmpty() || arr[i][0] > ans.get(ans.size() - 1).get(1)) {
+                ans.add(Arrays.asList(arr[i][0], arr[i][1]));
+            }
+            else {
+                ans.get(ans.size() - 1).set(1,Math.max(ans.get(ans.size() - 1).get(1), arr[i][1]));
+            }
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
-        int[][] intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
-        int[][] merge = merge(intervals);
-        System.out.println(merge);
+        int[][] intervals = {{1,3},{2,6},{8,10},{15,18}};
+//        int[][] merge = merge(intervals);
+        List<List<Integer>> lists = mergeOverlappingIntervals(intervals);
+        System.out.println(lists);
     }
 }
