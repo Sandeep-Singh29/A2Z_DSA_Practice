b51812a3a9691ec12c8263e80c385d318dcb85a2
Sandeep-Singh29 <singh29july95@gmail.com>
2024-08-22 23:48:49 +0530

    day 6 code night

--- src/com/practice/leetcode/string/RemoveOutermostparentheses_1021.java
+++ src/com/practice/leetcode/string/RemoveOutermostparentheses_1021.java
@@ -1,15 +1,53 @@
 package com.practice.leetcode.string;
 
+import java.util.Stack;
+
 public class RemoveOutermostparentheses_1021 {
 
     public static String removeOuterParentheses(String s) {
-
-        return "";
+        StringBuilder result = new StringBuilder();
+        int parenthesesCount = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            if (currentChar == '(') {
+                if (parenthesesCount > 0) {
+                    result.append(currentChar);
+                }
+                parenthesesCount++;
+            } else {
+                parenthesesCount--;
+                if (parenthesesCount > 0) {
+                    result.append(currentChar);
+                }
+            }
+        }
+        return result.toString();
     }
 
     public static void main(String[] args) {
-        String s = "(()())(())(()(()))";  // (()())(())
-        String string = removeOuterParentheses(s);
+        String s = "(()())(())";  // (()())(())
+//        String string = removeOuterParentheses(s);
+//        System.out.println(string);
+        String string = removeOuterParenthesesUsingStack(s);
         System.out.println(string);
+    }
+
+    private static String removeOuterParenthesesUsingStack(String s) {
+        StringBuilder stringBuilder= new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c=='('){
+                if(stack.size()>0){
+                    stringBuilder.append(c);
+                }
+                stack.push(c);
+            }else{
+                stack.pop();
+                if (stack.size()>0){
+                    stringBuilder.append(c);
+                }
+            }
+        }
+        return stringBuilder.toString();
     }
 }
---
+++ src/com/practice/leetcode/string/ReverseWord_151.java
@@ -0,0 +1,34 @@
+package com.practice.leetcode.string;
+
+public class ReverseWord_151 {
+    public static String reverseWords(String s) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String[] s1 = s.trim().split(" ");
+        for(int i=s1.length-1;i>=0;i--){
+            String data = s1[i].trim();
+            if (!data.isEmpty()){
+                stringBuilder.append(data.trim()).append(" ");
+            }
+        }
+        return stringBuilder.toString().trim();
+    }
+
+    public static void main(String[] args) {
+        String s ="the sky    is blue";
+//        String string = reverseWords(s);
+//        System.out.println(string);
+        String string = reverseW(s);
+        System.out.println(string);
+    }
+
+    public static String reverseW(String s) {
+        String res = "";
+        String[] split = s.split(" +");
+        for(int i=0; i<split.length;i++) {
+            res += " "+split[split.length-1-i];
+        }
+        return res.trim();
+    }
+}
+
+
