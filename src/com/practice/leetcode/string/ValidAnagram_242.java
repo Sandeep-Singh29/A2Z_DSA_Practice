5f1ce490bc0f4106dff3a582e88ac373862aef25
Sandeep <singh29july95@gmail.com>
2024-08-27 17:48:03 +0530

    day 8 code

--- src/com/practice/leetcode/string/ValidAnagram_242.java
+++ src/com/practice/leetcode/string/ValidAnagram_242.java
@@ -10,19 +10,36 @@ public class ValidAnagram_242 {
         Arrays.sort(sChars);
         Arrays.sort(tChars);
         return Arrays.equals(sChars, tChars);*/
-        int[] count = new int[26];
-        for (char ch : s.toCharArray()) {
-            count[ch - 'a']++;
+
+
+        // Way 3...
+//        int[] count = new int[26];
+//        for (char ch : s.toCharArray()) {
+//            count[ch - 'a']++;
+//        }
+//        for (char ch : t.toCharArray()) {
+//            count[ch - 'a']--;
+//        }
+//        boolean allZeros = Arrays.stream(count).allMatch(element -> element == 0);
+//        return allZeros;
+
+
+        // Way ... 3
+        String result = t;
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(result.contains(String.valueOf(c))){
+                String replace = result.replace(String.valueOf(c), "");
+                result = replace;
+            }else{
+                return false;
+            }
         }
-        for (char ch : t.toCharArray()) {
-            count[ch - 'a']--;
-        }
-        boolean allZeros = Arrays.stream(count).allMatch(element -> element == 0);
-        return allZeros;
+        return result.isEmpty();
     }
 
     public static void main(String[] args) {
-        String s = "rat", t = "car";
+        String s = "aacc", t = "ccac";
         boolean anagram = isAnagram(s, t);
         System.out.println(anagram);
     }
