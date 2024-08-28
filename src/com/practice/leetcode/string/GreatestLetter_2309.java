3e7f3e00cf9bddb57db75f6680b8c5a0ff846404
Sandeep-Singh29 <singh29july95@gmail.com>
2024-08-28 19:43:22 +0530

    code day 9

---
+++ src/com/practice/leetcode/string/GreatestLetter_2309.java
@@ -0,0 +1,29 @@
+package com.practice.leetcode.string;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class GreatestLetter_2309 {
+
+    public static String greatestLetter(String s) {
+        Set<Character> ss = new HashSet<>();
+        for (char c : s.toCharArray()) {
+            ss.add(c);
+        }
+        for (char a = 'Z'; a >= 'A'; --a) {
+            if (ss.contains(a) && ss.contains((char) (a + 32))) {
+                return String.valueOf(a);
+            }
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        String s = "lEeTcOdE";
+        String string = greatestLetter(s);
+        System.out.println(string);
+    }
+
+}
---
+++ src/com/practice/leetcode/string/IsomorphicString_205.java
@@ -0,0 +1,54 @@
+package com.practice.leetcode.string;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IsomorphicString_205 {
+
+    public static boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> map = new HashMap<>();
+        Map<Character, Character> check = new HashMap<>();
+        for(int i=0 ;i <s.length(); i++){
+            if(!map.containsKey(s.charAt(i))){
+                if(check.containsKey(t.charAt(i))){
+                    return false;
+                }
+                map.put(s.charAt(i),t.charAt(i));
+            }
+            if(!check.containsKey(t.charAt(i))){
+                check.put(t.charAt(i),s.charAt(i));
+            }
+            if(map.containsKey(s.charAt(i))){
+                String c = map.get(s.charAt(i)).toString();
+                if(!c.equals(String.valueOf(t.charAt(i)))){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean isIsomorphic2(String s, String t) {
+        Map<Character, Character> mp1 = new HashMap<>();
+        Map<Character, Character> mp2 = new HashMap<>();
+        int m = s.length();
+        for(int i = 0; i < m; i++) {
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+            if((mp1.containsKey(ch1) && mp1.get(ch1) != ch2) ||(mp2.containsKey(ch2) && mp2.get(ch2) != ch1)) {
+                return false;
+            }
+            mp1.put(ch1, ch2);
+            mp2.put(ch2, ch1);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "paper", t = "title";
+        boolean isomorphic = isIsomorphic(s, t);
+//        System.out.println(isomorphic);
+        boolean isomorphic2 = isIsomorphic2(s, t);
+        System.out.println(isomorphic2);
+    }
+}
