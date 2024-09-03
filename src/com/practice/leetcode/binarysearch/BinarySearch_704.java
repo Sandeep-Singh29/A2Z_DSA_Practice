67a29a2ccb0b55cc25e0f06e0bc8a5124c195117
Sandeep-Singh29 <singh29july95@gmail.com>
2024-09-03 21:50:49 +0530

    day 11 code

---
+++ src/com/practice/leetcode/binarysearch/BinarySearch_704.java
@@ -0,0 +1,35 @@
+package com.practice.leetcode.binarysearch;
+
+public class BinarySearch_704 {
+    public static int search(int[] nums, int target) {
+        int low = 0;
+         int high = nums.length - 1;
+        while (low < high) {
+//            int mid = (low + high) / 2;
+            int mid = low+(high-low)/2;
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target > nums[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {-1, 0, 3, 5, 9, 12}, target = 9;
+        int search = search(nums, target);
+        System.out.println(search);
+//        int res = searchUsingRecursive(nums, target);
+//        System.out.println(res);
+    }
+
+//    private static int searchUsingRecursive(int[] nums, int target) {
+//        if(){
+//
+//        }
+//    }
+
+}
