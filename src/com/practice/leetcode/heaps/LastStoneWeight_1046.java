633967ef2b6536cdd5e178474f69a2ff22012c99
Sandeep-Singh29 <singh29july95@gmail.com>
2024-10-08 23:16:29 +0530

    code day 33

--- src/com/practice/leetcode/heaps/LastStoneWeight_1046.java
+++ src/com/practice/leetcode/heaps/LastStoneWeight_1046.java
@@ -10,41 +10,45 @@ import java.util.*;
 public class LastStoneWeight_1046 {
 
     private static int lastStoneWeight(int[] stones) {
-        if(stones.length==1){
-            return stones[0];
+        List<Integer> list = new ArrayList<>();
+        for (int e : stones) {
+            list.add(e);
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for (int a : stones) {
-            pq.add(a);
-        }
-        System.out.println(pq);
-        List<Integer> list = new ArrayList<>(pq);
-        Set<Integer> set = new HashSet<>();
-        System.out.println(list);
-        int i = 0;
-        int j = 1;
-        while (list.size()>1) {
-            int x = list.get(i);
-            int y = list.get(j);
+        while (list.size() > 1) {
+            Collections.sort(list);
+            int x = list.remove(list.size() - 1);
+            int y = list.remove(list.size() - 1);
             if (x != y) {
                 list.add(x - y);
-                list.remove(i);
-                if(list.size()==1) return list.getFirst();
-                list.remove(i);
-            } else {
-                list.remove(i);
-                if(list.size()==1) return list.getFirst();
-                list.remove(i);
             }
-            Collections.sort(list.reversed());
         }
-        return list.getFirst();
+        if (list.size() == 0) {
+            return 0;
+        } else return list.get(0);
+    }
+
+    private static int lastStoneWeightway2Optiomal(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int e : stones){
+            pq.add(e);
+        }
+        while(pq.size()>1){
+            int x = pq.remove();
+            int y = pq.remove();
+            if(x!=y){
+                pq.add(x-y);
+            }
+        }
+        if(pq.size()==0) return 0;
+        return pq.remove();
     }
 
     public static void main(String[] args) {
-        int[] stones = {2, 2};
-        int stoneWeight = lastStoneWeight(stones);
-        System.out.println(stoneWeight);
+        int[] stones = {2,7,4,1,8,1};
+//        int stoneWeight = lastStoneWeight(stones);
+//        System.out.println(stoneWeight);
+        int lasted = lastStoneWeightway2Optiomal(stones);
+        System.out.println(lasted);
     }
 
 }
