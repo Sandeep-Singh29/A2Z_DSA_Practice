f1600d8fbd4709d7f7efa99ab701ed2d810a32b7
Sandeep <singh29july95@gmail.com>
2024-10-08 17:37:00 +0530

    code top Stone

--- src/com/practice/leetcode/heaps/LastStoneWeight_1046.java
+++ src/com/practice/leetcode/heaps/LastStoneWeight_1046.java
@@ -10,6 +10,9 @@ import java.util.*;
 public class LastStoneWeight_1046 {
 
     private static int lastStoneWeight(int[] stones) {
+        if(stones.length==1){
+            return stones[0];
+        }
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         for (int a : stones) {
             pq.add(a);
@@ -18,23 +21,28 @@ public class LastStoneWeight_1046 {
         List<Integer> list = new ArrayList<>(pq);
         Set<Integer> set = new HashSet<>();
         System.out.println(list);
-        for (int i = 1; i < list.size(); i++) {
-            int x = list.get(i - 1);
-            int y = list.get(i);
+        int i = 0;
+        int j = 1;
+        while (list.size()>1) {
+            int x = list.get(i);
+            int y = list.get(j);
             if (x != y) {
-                list.remove(i-1);
-                list.remove(i-1);
                 list.add(x - y);
+                list.remove(i);
+                if(list.size()==1) return list.getFirst();
+                list.remove(i);
             } else {
-                list.remove(i-1);
-                list.remove(i-1);
+                list.remove(i);
+                if(list.size()==1) return list.getFirst();
+                list.remove(i);
             }
+            Collections.sort(list.reversed());
         }
-        return list.get(0);
+        return list.getFirst();
     }
 
     public static void main(String[] args) {
-        int[] stones = {2, 7, 4, 1, 8, 1};
+        int[] stones = {2, 2};
         int stoneWeight = lastStoneWeight(stones);
         System.out.println(stoneWeight);
     }
