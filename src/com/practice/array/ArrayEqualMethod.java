0fece50caa318953670343c0e5a1d8ad273a3b6b
Sandeep <singh29july95@gmail.com>
2025-05-22 14:29:56 +0530

    test code

--- src/com/practice/array/ArrayEqualMethod.java
+++ src/com/practice/array/ArrayEqualMethod.java
@@ -9,6 +9,8 @@ import java.util.Arrays;
 
 public class ArrayEqualMethod {
 
+
+
     public static void main(String[] args) {
         int a[] = {1, 2, 3, 4, 5};
         int b[] = {4, 2, 3, 5, 1};
